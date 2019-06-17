c9d99417f3ca70144f998d087cb536ee9e283318
magmagan <alexlalves@live.com>
2019-06-16 21:17:44 -0300

    add languagewriter abstract class

---
+++ Visitors/LanguageWriter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrimsonForthCompiler.Visitors {
+    abstract class LanguageWriter {
+
+        public abstract void WriteExpression(string operand);
+
+    }
+}
