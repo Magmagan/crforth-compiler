920f2506828d521610c8c9119a43172c8772758e
magmagan <alexlalves@live.com>
2019-06-17 20:22:47 -0300

    another label generator

---
+++ Visitors/CrimsonForthVisitor/LabelGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrimsonForthCompiler.Visitors.CrimsonForthVisitor {
+
+    class LabelGenerator {
+
+        int internalCount;
+
+        public LabelGenerator() {
+            this.internalCount = 0;
+        }
+
+        public string GenerateGenericLabel() {
+            return $"LBL_GENERIC_{this.internalCount++}";
+        }
+
+        public string GenerateIfLabel() {
+            return $"LBL_IF_{this.internalCount++}";
+        }
+
+        public string GenerateWhileLabel() {
+            return $"LBL_WHILE_{this.internalCount++}";
+        }
+
+        public string GenerateFunctionLabel(string functionName) {
+            return $"LBL_FN_{functionName}";
+        }
+
+    }
+}
